004597b44b2d043f3e6680588c594fc0f01abc97
knezevicvasilije <73421159+knezevicvasilije@users.noreply.github.com>
2021-02-02 20:26:02 +0100

    PropTypes dependency fixed (sandbox)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
-import PropTypes from 'prop-types';
-import Button from './Button';
-
-const Header = ({title, onAdd, showAdd}) => {
- 
-    return (
-        <header className='header'>
-        <h1>{title}</h1>
-        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
-        </header>
-    )
-}
+import PropTypes from "prop-types";
+import Button from "./Button";
+/*Ukoliko bude javljalo gresku za sandBox potrebno je  
+prihvatiti solution za dodavanje proptypes dependecy
+*/
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
 Header.defaultProps = {
-    title: 'Task Tracker'
-}
+  title: "Task Tracker"
+};
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired
+};
 
-export default Header
+export default Header;
